b3224bc18e3385b8afe7e0d5e1214607ea79c90c
Isac Kim <isackim91@yahoo.com>
2018-04-13 18:46:14 -0500

    scrollspy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,18 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function (){
+
+    function initialize() {
+      var mapOptions = {
+      center: { lat: 29.497082, lng: -98.412727 },
+      zoom: 12
+      };
+        
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);    
+    }
+      
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
